8006bb0fbb054057463ee25cf51439294403d4ca
Wanderli Eckel <wanderli_eckel@hotmail.com>
2023-02-20 17:39:16 -0300

    teste fsuipc

--- fsuipc.js
+++ fsuipc.js
@@ -1,25 +1,24 @@
 import fsuipcWasm from 'fsuipc-wasm';
+import { VarstoRead } from './Modules/Switches.js'
 
-const obj = new fsuipcWasm.FSUIPCWASM({
-    // logLevel: fsuipcWasm.LogLevel.Enable,
+var fsuipcvars = {}
+
+VarstoRead.a320fenix.forEach((value, index) => {
+  const obj = new fsuipcWasm.FSUIPCWASM({});
+  test()
+})
+
+
+
+
+
+async function test(value) {
+  await obj.start();
+  obj.flagLvarForUpdate(value);
+
+  obj.setLvarUpdateCallback((newLvars) => {
+    console.log(newLvars);
+    fsuipcvars[value] = obj
   });
-  
-  async function test() {
-    await obj.start();
-  
-    console.log('Started');
-  
-    console.log(obj.lvarValues);
-    console.log(obj)
-  
-    obj.flagLvarForUpdate("I_OH_ELEC_EXT_PWR_U");
-  
-    obj.setLvarUpdateCallback((newLvars) => {
-      console.log(newLvars);
-      
-    });
-  
-    // await obj.close();
-  }
-  
-  test().catch(e => console.error(e));
+  // await obj.close();
+}
