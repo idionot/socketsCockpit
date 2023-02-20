0158933a0a869f82f44df7f5c7ec8a1ed18eaa7d
Wanderli Eckel <wanderli_eckel@hotmail.com>
2023-02-20 15:12:04 -0300

    Inserção do teste fsuipc Wasm

---
+++ fsuipc.js
@@ -0,0 +1,23 @@
+import fsuipcWasm from 'fsuipc-wasm';
+
+const obj = new fsuipcWasm.FSUIPCWASM({
+    // logLevel: fsuipcWasm.LogLevel.Enable,
+  });
+  
+  async function test() {
+    await obj.start();
+  
+    console.log('Started');
+  
+    console.log(obj.lvarValues);
+  
+    obj.flagLvarForUpdate("A32NX_IS_STATIONARY");
+  
+    obj.setLvarUpdateCallback((newLvars) => {
+      console.log(newLvars);
+    });
+  
+    // await obj.close();
+  }
+  
+  test().catch(e => console.error(e));
